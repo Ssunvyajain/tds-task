6ecfe2ce2093c76f5c5e2790f7dfa01f7cfe98c8
Ssunvyajain <23f2005564@ds.study.iitm.ac.in>
2026-06-21 16:54:42 +0530

    Update analytics.js

--- api/analytics.js
+++ api/analytics.js
@@ -1,1 +1,8 @@
 
+export default function handler(req, res) {
+  if (req.method === "POST") {
+    return res.status(200).json({ message: "success" });
+  }
+
+  return res.status(405).json({ error: "Only POST allowed" });
+}
